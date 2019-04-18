85d28b971ea6db79c6dbd490b2ea994380c5c62f
lackadaisicalcynic <mikle.moiseev@mail.ru>
2019-04-18 21:59:27 +0300

    Task 3

--- src/ObjectBox.java
+++ src/ObjectBox.java
@@ -3,8 +3,11 @@ import java.util.ArrayList;
 import static java.lang.String.valueOf;
 
 public class ObjectBox {
-    private ArrayList<Object> list = new ArrayList<>();
+
+    private ArrayList<Object> list;
+
     public ObjectBox(Object[] objects){
+        list = new ArrayList<>();
         for(int i = 0; i < objects.length; i++){
             this.list.add(objects[i]);
         }
@@ -13,14 +16,22 @@ public class ObjectBox {
     public void addObject(Object object){
         list.add(object);
     }
+
     public void deleteObject(Object object){
         list.remove(Double.parseDouble(valueOf(object)));
     }
+
     public String dump(){
         String str = "";
-        for (int i = 0; i < this.list.size(); i++){
-            str += this.list.get(i).toString() + " ";
+        for (int i = 0; i < list.size(); i++){
+            str += list.get(i).toString() + " ";
         }
         return str.trim();
     }
+
+//  getter
+    public ArrayList<Object> getList(){
+        return list;
+    }
+
 }
